349f6dc4d044ee7195648bfe9c01dbfc5845cccc
Thamiris <102323316+ThamirisLisboa@users.noreply.github.com>
2022-04-06 21:03:18 -0300

    Add files via upload

---
+++ AviaoMain.java
@@ -0,0 +1,14 @@
+package fundamentosaviao;
+
+public class AviaoMain {
+ public static void main (String []args) {
+	 Exercicio2 Exercicio = new Exercicio2();
+	 
+	 System.out.println();
+	 Exercicio.marca = "AirbusA330";
+	 Exercicio.cor = "branco";
+	 Exercicio.QuantLugares = 240;
+	 
+	 Exercicio.imprimir();
+ }
+}
---
+++ Exercicio2.java
@@ -0,0 +1,25 @@
+//2) Crie uma classe avi e apresente os atributos e métodos referentes//
+//esta classe, em seguida crie um objeto avião, defina as instancias deste//
+//objeto e apresente as informações deste objeto no console.//
+
+package fundamentosaviao;
+
+ public class Exercicio2 {
+	 String marca, cor ;
+	 int QuantLugares; 
+	  
+	 public void marca ( ) {
+	 	this.marca = marca; 		 			
+ }
+	public void cor () {
+	 this.cor = cor;
+ }
+	public void QuantLugares () {
+		this.QuantLugares = QuantLugares;
+	}
+		public void imprimir () {
+		System.out.println("marca: " + this.marca);
+		System.out.println("cor: " + this.cor);
+		System.out.println("Lugares: " + this.QuantLugares );
+	}
+ }
